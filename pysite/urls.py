83b19129e8b66d4f044e8f01f014ba7b85e9c612
Luke Dupin fsd <you@example.com>
2016-02-03 22:42:15 +0000

    Fixed the static url serve resolve

--- pysite/urls.py
+++ pysite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -27,4 +29,5 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
